451b9ab9ab944a25d11d10e8c1f16db2fc19cdb9
ethanwatsonj88 <ethanwatsonj88@gmail.com>
2019-04-06 15:37:09 -0400

    working commit before adding follower join table

--- lib/project2/users/user.ex
+++ lib/project2/users/user.ex
@@ -9,13 +9,15 @@ defmodule Project2.Users.User do
 		
 		has_many :songs, Project2.Songs.Song
 
+		has_many :followers, Project2.Users.User
+
     timestamps()
   end
 
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :username, :admin])
-    |> validate_required([:email, :username, :admin])
+    |> cast(attrs, [:email, :username, :admin, :songs, :followers])
+    |> validate_required([:email, :username, :admin, :songs, :followers])
   end
 end
