8dda4ee89610ca0dfe8df8f6c82b2f6f36d9c836
Justin <xia.ju@husky.neu.edu>
2019-04-08 05:10:10 -0400

    added uniqueness constraint to follows

--- lib/project2/follows/follow.ex
+++ lib/project2/follows/follow.ex
@@ -1,6 +1,8 @@
 defmodule Project2.Follows.Follow do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Project2.Users.User
+  alias Project2.Follows
 
   schema "follows" do
     ##field :follower_id, :id
@@ -18,5 +20,6 @@ defmodule Project2.Follows.Follow do
     follow
     |> cast(attrs, [:follower_id, :following_id])
     |> validate_required([:follower_id, :following_id])
+    |> unique_constraint(:follower_id, name: :follows_follower_id_following_id_index)
   end
 end
--- priv/repo/migrations/3_create_follows.exs
+++ priv/repo/migrations/3_create_follows.exs
@@ -3,13 +3,12 @@ defmodule Project2.Repo.Migrations.CreateFollows do
 
   def change do
     create table(:follows) do
-      add :follower_id, references(:users, on_delete: :nothing)
-      add :following_id, references(:users, on_delete: :nothing)
+      add :follower_id, references(:users, on_delete: :delete_all)
+      add :following_id, references(:users, on_delete: :delete_all)
 
       timestamps()
     end
 
-    create index(:follows, [:follower_id])
-    create index(:follows, [:following_id])
+    create unique_index(:follows, [:follower_id, :following_id])
   end
 end
